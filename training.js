0aae5679f062dcdecc651d63d56956abb2aa4109
deniSwd <denis_swd@mail.ru>
2022-01-08 17:25:44 +0300

    08.01.2022 codeWars Calculate BMI

--- training.js
+++ training.js
@@ -1,8 +1,13 @@
-function repeatStr (n, s) {
-    let b ='';
-    for (let i = 1;  i<=n; i++) {
-        b=b+s;
+function bmi(weight, height) {
+    let bmi = weight / height**2;
+    if (bmi <=18.5) {
+        return ("Underweight")
+    } else if (bmi <=25.0) {
+        return ("Normal")
+    } else if (bmi <=30.0) {
+        return ("Overweight")
+    } else if (bmi >30.0) {
+        return ("Obese")
     }
-    return b ;
 }
-repeatStr (5, '*');
+bmi(70, 175);
