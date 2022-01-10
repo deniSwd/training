def358387c0d503772d9581dc4989d736082b6a1
deniSwd <denis_swd@mail.ru>
2022-01-10 22:32:04 +0300

    10.01.2022 codeWars quarterOf

--- training.js
+++ training.js
@@ -1,10 +1,14 @@
-let factorialize = (num) => {
-    if (num ==1) {
-        return num
-    } else if (num ==0) {
+const quarterOf = (month) => {
+    if(month >= 1 && month <= 3) {
         return 1
+    } else if (month >= 4 && month <= 6) {
+        return 2
+    } else if (month >= 7 && month <= 9) {
+        return 3
+    } else if (month >= 10 && month <= 12) {
+        return 4
     } else {
-        return num *factorialize(num-1)
+        return "hi"
     }
 }
-console.log(factorialize(5));;
+console.log(quarterOf(5))
