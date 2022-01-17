521140629844c886ed48b96349f94fc4a137a5f9
deniSwd <denis_swd@mail.ru>
2022-01-17 22:10:34 +0300

    17.01.2022 codeWars collisionDetection

--- training.js
+++ training.js
@@ -1,7 +1,7 @@
-function cookingTime(eggs) {
-    let time =0
-    let a = eggs % 8 == 0 ? time = eggs / 8 * 5 : time = ((eggs - eggs % 8) + 8) / 8 * 5
-    return time
+function collision(x1, y1, radius1, x2, y2, radius2) {
+    let a = x2-x1
+    let b = y2-y1
+    let d = Math.sqrt (Math.pow(a, 2)+Math.pow(b,2))
+    let c = radius1 + radius2
+    return c > d
 }
-
-console.log(cookingTime(46))
