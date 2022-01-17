e20c2df5ed89d22573b749235959aa11d682f137
deniSwd <denis_swd@mail.ru>
2022-01-17 22:32:56 +0300

    17.01.2022 codeWars listFiltering

--- training.js
+++ training.js
@@ -1,7 +1,7 @@
-function collision(x1, y1, radius1, x2, y2, radius2) {
-    let a = x2-x1
-    let b = y2-y1
-    let d = Math.sqrt (Math.pow(a, 2)+Math.pow(b,2))
-    let c = radius1 + radius2
-    return c > d
+function filter_list(l) {
+    let a = []
+    for (let i = 0; i <= l.length - 1; i++) {
+        typeof l[i]=="number" ? a.push(l[i]) : a
+    }
+    return a
 }
