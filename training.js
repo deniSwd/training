d8bb8a31448097f14591aaeac2f022b85c799ecc
deniSwd <denis_swd@mail.ru>
2022-02-06 17:00:11 +0300

    06.02.2022 codeWars My Array.from

--- training.js
+++ training.js
@@ -1,17 +1,9 @@
-function lastDigit(n, d) {
-    let result= []
-    let string = String(n)
-    if (d >= string.length){
-        for (let i=0; i<=string.length-1; i++)
-            result.push(Number(string[i]))
-        return result
-    } else if (d > 0 || d< string.length) {
-        for (let i=d; i>=1; i--)
-            result.push(Number(string[string.length-i]))
-        return result
-    } else {
-        return []
+function digitize(n) {
+    let ns= String(n)
+    let mas =[]
+    for(let i =0; i<= ns.length -1; i++){
+        mas.push(Number(ns[i]))
     }
-
+    return mas
 }
-console.log (lastDigit(12345, 9) )
+console.log(digitize(2569))
