77b292bde4524fbd833d83e82bed38d296d58b09
deniSwd <denis_swd@mail.ru>
2022-05-31 00:38:12 +0300

    31.05.2022 'Coprime'Validation

--- training.js
+++ training.js
@@ -1,11 +1,33 @@
-const unusedDigits = (...arg) => {
-    let newString = ""
-    const myString = String(arg)
-    for (let i = 0; i < 10; i++) {
-        if (!myString.includes(i)) {
-            newString += i
+const isCoprime = (x, y) => {
+    const divisors = (number) => {
+        let myDivisors = []
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) {
+                myDivisors.push(i)
+            }
         }
+        console.log(myDivisors)
+        return myDivisors
     }
-    return newString
+    let firstNumberArr = divisors(x)
+    let secondNumberArr = divisors(y)
+    let arrayForTest
+    let arrayForFind
+    if (firstNumberArr.length > secondNumberArr.length) {
+        arrayForTest = secondNumberArr
+        arrayForFind = firstNumberArr
+    } else {
+        arrayForTest = firstNumberArr
+        arrayForFind = secondNumberArr
+    }
+    let lagerDivider
+    for (let a = 1; a < arrayForTest.length; a++) {
+        lagerDivider = arrayForFind.find(v => v === arrayForTest[a])
+        if (lagerDivider) break
+    }
+    if (lagerDivider === undefined) {
+        return console.log('true')
+    }
+    return console.log('false')
 }
-unusedDigits(12, 34, 56, 78)
+isCoprime(44,88)
