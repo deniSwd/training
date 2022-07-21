4734f12596df446d51eb2dcfe3c56e2d5fd74b67
deniSwd <denis_swd@mail.ru>
2022-07-21 12:00:17 +0200

    21.07.2022 redistributeWealth ES6

--- training.js
+++ training.js
@@ -1,1 +1,5 @@
-const descendingOrder = n => Number(n.toString().split('').sort().reverse().join(''))
+const redistributeWealth = wealth => {
+    const equalyWealth = wealth.reduce((sum, i) => sum + i, 0) / wealth.length
+    return console.log(wealth.fill(equalyWealth, 0, wealth.length))
+}
+redistributeWealth([2, 1, 5, 7, 10, 11, 7])
